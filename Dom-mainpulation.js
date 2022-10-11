2ad1def3545c61a95cfe64b8069e62858c9861a1
David Zambrano <david._101@hotmail.com>
2022-10-11 09:35:39 -0500

    Fix name error and linter

--- Dom-mainpulation.js
+++ Dom-mainpulation.js
@@ -8,7 +8,7 @@ function mobileMenu() {
 document.getElementById('hamb-btn').addEventListener('click', mobileMenu);
 document.getElementById('close').addEventListener('click', mobileMenu);
 const anchorElm = document.querySelectorAll('.pop-window');
-/* eslint-disable-next-line no-plusplus */
+
 for (let i = 0; i < anchorElm.length; i += 1) {
   anchorElm[i].addEventListener('click', mobileMenu);
 }
