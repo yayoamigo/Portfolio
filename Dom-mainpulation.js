db13e6667be11736b6022b93a754071987755ae7
David Zambrano <david._101@hotmail.com>
2022-10-13 22:16:44 -0500

    Fix email input

--- Dom-mainpulation.js
+++ Dom-mainpulation.js
@@ -28,7 +28,7 @@ const ProjectData = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     descriptionD: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     image: '/tonicphoto.png',
-    imageD: '/desktopjs.png',
+    imageD: '/1desktop.png',
     technologies: ['html', 'css', 'javascript'],
     technologiesD: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     linklive: 'https://yayoamigo.github.io/mobile-template/',
@@ -560,7 +560,6 @@ function validateImputs() {
     setErrorForBtn(errorBtn, 'All inputs must be valid');
   } else {
     setSuccessFor(textarea);
-    setSuccessFor(errorBtn);
   }
 }
 
